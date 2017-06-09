7ddc853a8881c17576c7b65b6ab32c7084db56a5
Stoeffel <schtoeffel@gmail.com>
2017-06-09 18:53:03 +0200

    fix run tests

--- run-tests.sh
+++ run-tests.sh
@@ -1,11 +1,11 @@
 #! /bin/bash -ex
-TEST_COUNT = cat example/tests/Doc/**/*.elm | grep 'Test.test' | wc -l
+TEST_COUNT=`find ./example/tests/Doc -name "*.elm" | xargs cat | grep 'Test.test' | wc -l`
 
 elm-make src/DocTest.elm --output bin/elm.js &&
 cd example &&
 ../bin/cli.js &&
 {
-  elm-test | grep "Passed:   $TEST_COUNT" &&
+  elm-test | grep "Passed" | grep $TEST_COUNT &&
   echo "👍"
 } || {
   echo "Expected $TEST_COUNT passing specs!"
