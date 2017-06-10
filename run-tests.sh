496e58feff9b2ed1f49ead21603836888dd5932b
Stoeffel <schtoeffel@gmail.com>
2017-06-10 13:57:15 +0200

    hard code for simplicity

--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,5 @@
 #! /bin/bash -ex
-TEST_COUNT=`find ./example/tests/Doc -name "*.elm" | xargs cat | grep 'Test.test' | wc -l`
+TEST_COUNT=11
 
 elm-make src/DocTest.elm --output bin/elm.js &&
 cd example &&
