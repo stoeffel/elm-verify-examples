d8a44ba46e01a31aac0d4e3a7c997fa75b1c24ef
Christoph Hermann <schtoeffel@gmail.com>
2017-06-16 15:05:40 +0200

    fix travis (WIP) (#29)
    * attempt
    
    * fix
    
    * &&

--- run-tests.sh
+++ run-tests.sh
@@ -1,14 +1,10 @@
 #! /bin/bash -ex
 TEST_COUNT=15
 
-elm-make src/DocTest.elm --output bin/elm.js &&
-cd example &&
-../bin/cli.js &&
-{
-  elm-test | grep "Passed" | grep $TEST_COUNT &&
-  echo "👍"
-} || {
-  echo "Expected $TEST_COUNT passing specs!"
-  npm start
-  exit -1
-}
+elm-make src/DocTest.elm --output bin/elm.js
+pushd example
+../bin/cli.js
+elm-test 2>&1 | tee ../result.json
+popd
+cat result.json | grep "Passed:   ${TEST_COUNT}"
+cat result.json | grep "Failed:   0"
