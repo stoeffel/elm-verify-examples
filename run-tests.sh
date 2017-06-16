2469ec2e343275c699f572a3b13db9ece2e7bedd
Stoeffel <schtoeffel@gmail.com>
2017-06-16 13:44:41 +0200

    attempt

--- run-tests.sh
+++ run-tests.sh
@@ -1,11 +1,12 @@
 #! /bin/bash -ex
 TEST_COUNT=15
 
-elm-make src/DocTest.elm --output bin/elm.js &&
-cd example &&
-../bin/cli.js &&
+elm-make src/DocTest.elm --output bin/elm.js
+cd example
+../bin/cli.js
+
 {
-  elm-test | grep "Passed" | grep $TEST_COUNT &&
+  ./node_modules/.bin/elm-test | grep "Passed" | grep $TEST_COUNT
   echo "👍"
 } || {
   echo "Expected $TEST_COUNT passing specs!"
