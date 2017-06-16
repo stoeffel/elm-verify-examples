404dcd7f01662bf2a99838750310e632d098ce4f
Stoeffel <schtoeffel@gmail.com>
2017-06-16 14:23:32 +0200

    &&

--- run-tests.sh
+++ run-tests.sh
@@ -2,15 +2,9 @@
 TEST_COUNT=15
 
 elm-make src/DocTest.elm --output bin/elm.js
-cd example
+pushd example
 ../bin/cli.js
-
-{
-  elm-test > ../result.json
-  cd ..
-  cat result.json | grep "Passed:   ${TEST_COUNT}"
-  cat result.json | grep "Failed:   0"
-} || {
-  echo "Expected $TEST_COUNT passing specs!"
-  exit -1
-}
+elm-test 2>&1 | tee ../result.json
+popd
+cat result.json | grep "Passed:   ${TEST_COUNT}"
+cat result.json | grep "Failed:   0"
