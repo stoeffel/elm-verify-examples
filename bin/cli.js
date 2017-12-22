65e0f51e6c8542aec74435e6a63784aa21fdc425
Stoeffel <schtoeffel@gmail.com>
2017-12-22 17:17:20 +0100

    depend on elm-test

--- bin/cli.js
+++ bin/cli.js
@@ -6,9 +6,19 @@ process.title = processTitle;
 
 var argv = require('yargs').argv;
 var init = require('./runner').init;
+var childProcess = require('child_process');
+var path = require('path');
 
 
 // stateful things
 var cliModel = init(argv);
 
-cliModel.run(cliModel);
+cliModel.run(cliModel, function() {
+  var exit = childProcess.spawnSync(path.join(__dirname, '../node_modules/.bin/elm-test'),
+    {
+      cwd: process.cwd(),
+      stdio: 'inherit'
+    });
+  cliModel.cleanup(cliModel);
+  process.exit(exit.status);
+});
--- run-tests.sh
+++ run-tests.sh
@@ -3,8 +3,7 @@ TEST_COUNT=18
 
 elm-make src/VerifyExamples.elm --output bin/elm.js --warn
 pushd example
-../bin/cli.js
-elm-test 2>&1 | tee ../result.json
+../bin/cli.js 2>&1 | tee ../result.json
 popd
 cat result.json | grep "Passed:   ${TEST_COUNT}"
 cat result.json | grep "Failed:   0"
