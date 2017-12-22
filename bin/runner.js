0b3273284318c7961eba01a438aa7bdc87a08035
Christoph Hermann <schtoeffel@gmail.com>
2017-12-22 21:05:25 +0100

    remove /tests/Doc before generating and run on single module (#50)
    * remove /tests/Doc before generating
    
    * verify examples in single module
    
    * run on single module
    
    * run on multiple modules
    
    * document
    
    * only one mode
    
    * remove unnecessary code
    
    * renaming
    
    * no `--run` necessary
    
    * flip condition

--- bin/runner.js
+++ bin/runner.js
@@ -4,51 +4,17 @@ var mkdirp = require("mkdirp");
 var fs = require("fs");
 var Elm = require("./elm.js");
 var helpers = require('./cli-helpers.js');
-
-
-
-/* Running modes currently supported are either
-   - generate, to generate tests as part of your suite
-   - run, to run a single file, provide the file name
-*/
-var RUNNING_MODE = {
-  GENERATE: 0,
-  RUN: 1
-};
-
-// runners are called via `.run` on a model
-var running_mode_runners = {};
-running_mode_runners[RUNNING_MODE.GENERATE] = generate;
-running_mode_runners[RUNNING_MODE.RUN] = run;
+var rimraf = require('rimraf');
 
 
 // loaders are called by init
-var running_mode_loaders = {};
-
-running_mode_loaders[RUNNING_MODE.GENERATE] = function(options){
+var model = function(options){
   var verifyExamplesConfig = helpers.loadVerifyExamplesConfig(options.configPath);
+  var config = forFiles(verifyExamplesConfig, options.forFiles);
 
   return {
-    runningMode: RUNNING_MODE.GENERATE,
-    config: verifyExamplesConfig,
-    run: running_mode_runners[RUNNING_MODE.GENERATE],
-    showWarnings: options.showWarnings,
-    output: options.output,
-    configPath: options.configPath
-  };
-};
-
-running_mode_loaders[RUNNING_MODE.RUN] = function(argv, options){
-  var files = argv.run;
-
-  var config = {
-    files: files
-  };
-
-  return {
-    runningMode: RUNNING_MODE.RUN,
     config: config,
-    run: running_mode_runners[RUNNING_MODE.RUN],
+    run: generate,
     showWarnings: options.showWarnings,
     output: options.output,
     configPath: options.configPath
@@ -58,13 +24,13 @@ running_mode_loaders[RUNNING_MODE.RUN] = function(argv, options){
 
 // parse args
 function init(argv){
-  var model = null;
   var defaultConfigPath = path.join(process.cwd(), 'tests/elm-verify-examples.json');
 
   var options = {
     showWarnings: true,
     output: "tests",
-    configPath: defaultConfigPath
+    configPath: defaultConfigPath,
+    forFiles: undefined
   };
 
   if (typeof argv.warn !== "undefined") {
@@ -79,26 +45,12 @@ function init(argv){
     options.configPath = argv.config;
   }
 
-  if (typeof argv.run === "undefined") {
-    if (options.showWarnings) console.log('Running in generate mode..');
-    model = running_mode_loaders[RUNNING_MODE.GENERATE](options);
-  } else {
-    if (options.showWarnings) console.log('Running in run mode..');
-    model = running_mode_loaders[RUNNING_MODE.RUN](argv, options);
+  if (typeof argv._ !== "undefined" && argv._.length > 0) {
+    options.forFiles = argv._;
   }
 
-  return model;
-}
-
-function run(model){
-  var config = model.config;
-  var files = config.files.split(' ');
-  files = files.filter(
-    function(v){ return v.endsWith('.elm'); }
-  ).map(elmPathToModule);
-
-
-  console.log(files);
+  if (options.showWarnings) console.log('Running in generate mode..');
+  return model(options);
 }
 
 function generate(model, allTestsGenerated) {
@@ -108,6 +60,7 @@ function generate(model, allTestsGenerated) {
     "tests"
   );
   var testsDocPath = path.join(model.output, "Doc");
+  rimraf.sync(testsDocPath);
 
   if (config.tests.length === 0){
     if (model.showWarnings) {
@@ -146,6 +99,18 @@ function generate(model, allTestsGenerated) {
         }
     });
   });
+}
+
+function forFiles(config, files){
+  if (typeof files === "undefined") {
+    return config;
+  }
+
+  config.tests = files.filter(
+    function(v){ return v.endsWith('.elm'); }
+  ).map(elmPathToModule);
+
+  return config;
 }
 
 function serial(xs, f, done) {
@@ -204,7 +169,10 @@ function writeFile(testsDocPath) {
 }
 
 function elmPathToModule(pathName){
-  return pathName.substr(0, pathName.length - 4).replace("/", ".");
+  if (pathName.startsWith("./")) {
+    pathName = pathName.substr(2);
+  }
+  return pathName.substr(0, pathName.length - 4).replace(/\//g, ".");
 }
 
 function elmModuleToPath(moduleName){
@@ -212,6 +180,5 @@ function elmModuleToPath(moduleName){
 }
 
 module.exports = {
-  RUNNING_MODE: RUNNING_MODE,
   init: init
 };
---
+++ example/NotInConfig.elm
@@ -0,0 +1,14 @@
+module NotInConfig exposing (..)
+
+{-| run `elm-verify-examples ./NotInConfig.elm && elm-test` in this folder
+-}
+
+
+{-|
+
+    hello --> "wolrd"
+
+-}
+hello : String
+hello =
+    "world"
