4e85652f655e452e11af5407215c57601e968583
Stoeffel <schtoeffel@gmail.com>
2017-12-30 21:35:26 +0100

    run elm-verify-examples on itself

--- src/String/Util.elm
+++ src/String/Util.elm
@@ -3,17 +3,47 @@ module String.Util exposing (escape, indent, indentLines, unlines)
 import Regex exposing (HowMany(..), Regex, regex)
 
 
+{-| Joins strings with a newline.
+
+    unlines
+        [ "a"
+        , "b"
+        , "c"
+        ]
+    --> "a\nb\nc"
+
+-}
 unlines : List String -> String
 unlines =
     String.join "\n"
 
 
+{-| Indents a string by steps of 4 characters.
+
+    indent 1 "hello" --> "    hello"
+
+    indent 2 "hello" --> "        hello"
+
+-}
 indent : Int -> String -> String
 indent count str =
     String.concat
         (List.repeat (count * 4) " " ++ [ str ])
 
 
+{-| Indent all lines in a string.
+
+    indentLines 1 <|
+        unlines
+            [ "|"
+            , "|"
+            ]
+    --> unlines
+    -->     [ "    |"
+    -->     , "    |"
+    -->     ]
+
+-}
 indentLines : Int -> String -> String
 indentLines count =
     String.lines
