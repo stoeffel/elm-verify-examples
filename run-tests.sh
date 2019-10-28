8c404b3dd5122c93d5da0c44b7dd9b17650df717
Stoeffel <schtoeffel@gmail.com>
2019-10-28 11:24:29 +0100

    fix run-tests

--- run-tests.sh
+++ run-tests.sh
@@ -6,8 +6,8 @@ TODO_COUNT=1
 npx elm make src/VerifyExamples.elm --output bin/elm.js --optimize
 pushd example
 # Push test output into a file, dropping all color codes in the process.
-../bin/cli.js --run-tests 2>&1 | sed 's/\x1b\[[0-9;]*m//g' | tee ../output.txt
+../bin/cli.js --run-tests 2>&1 | tee ../output.txt
 popd
-cat output.txt | grep "Passed:   ${PASSED_COUNT}"
-cat output.txt | grep "Failed:   ${FAILED_COUNT}"
-cat output.txt | grep "Todo:     ${TODO_COUNT}"
+cat output.txt | grep "Passed" | grep "${PASSED_COUNT}"
+cat output.txt | grep "Failed" | grep "${FAILED_COUNT}"
+cat output.txt | grep "Todo" | grep "${TODO_COUNT}"
